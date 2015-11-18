f4baf3f5484f2ddb0ee7be4da31605be1cdb62c1
Diego Echeverri <diego.echeverri@wooga.com>
2015-11-18 18:38:31 -0500

    Adding simple README

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# break-kraut
-Simple copy of breakout inspired by the http://haxeflixel.com/demos/Breakout/ demo. Adding tweens and score.
+# Break-Kraut
+
+Simple copy of breakout inspired by the http://haxeflixel.com/demos/Breakout/ demo.
+Adding tweens score, and sound.
+
+# Run the project:
+
+This project uses HaxeFlixel http://haxeflixel.com/
+
+```
+        $ haxelib run lime test neko
+```
+
+Will compile and run the project using the Neko virtual machine.
--- source/LostState.hx
+++ source/LostState.hx
@@ -8,11 +8,20 @@ import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
 import flixel.util.FlxRandom;
 import flixel.util.FlxColor;
-
 import flixel.text.FlxText;
+import haxe.Http;
+import haxe.Json;
 
 class LostState extends FlxState {
+  // Local:
+  // private static inline var BACKEND = "http://localhost:4000/";
+  // Production:
+  private static inline var BACKEND = "http://fast-everglades-7042.herokuapp.com/";
+
   var score: Int;
+  var rankingNameList: FlxText;
+  var rankingPointsList: FlxText;
+  var ranking: Array<Dynamic>;
 
   public function new(score: Int) {
     this.score = score;
@@ -23,8 +32,18 @@ class LostState extends FlxState {
     FlxG.mouse.visible = true;
     add(new FlxText(0, 5,  100, "R to Restart"));
     add(new FlxText(0, 15, 100, "Q to Quit"));
-    add(new FlxText(35, 100, 300, "YOU LOST!", 40));
-    add(new FlxText(140, 150, 300, "SCORE: " + score));
+
+    rankingNameList = new FlxText(110, 40, 300, "Loading...");
+    rankingPointsList = new FlxText(190, 40, 300, "");
+
+    add(rankingNameList);
+    add(rankingPointsList);
+
+    add(new FlxText(30, 120, 300, "YOU LOST!", 40));
+    add(new FlxText(140, 170, 300, "SCORE: " + score));
+
+    ranking = null;
+    doRankingCall();
   }
 
   override public function destroy():Void {
@@ -35,6 +54,17 @@ class LostState extends FlxState {
     FlxG.camera.fade(FlxColor.BLACK,.33, false, function() {
       FlxG.switchState(new PlayState());
     });
+  }
+
+  private function doRankingCall() {
+    var rankingCall: Http = new haxe.Http(BACKEND + "api/ranking");
+    rankingCall.onError = function (error: String) {
+      trace("Error getting the ranking data");
+    }
+    rankingCall.onData = function (data: String) {
+      ranking = Json.parse(data);
+    }
+    rankingCall.request();
   }
 
   override public function update():Void {
@@ -53,5 +83,18 @@ class LostState extends FlxState {
       }
     }
     #end
+
+    if(ranking != null) {
+      var names = "";
+      var points = "";
+      for(rank in ranking) {
+        names += "\n" + rank.name;
+        points += "\n" + rank.points;
+      }
+      this.rankingNameList.text = names;
+      this.rankingPointsList.text = points;
+
+      ranking = null;
+    }
   }
 }
